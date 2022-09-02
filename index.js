42b4c114bd89d311c27309963bd3993b25d043fc
Caro Paz <pazcaro979@gmail.com>
2022-09-02 20:05:52 -0300

    clima con api

---
+++ index.js
@@ -0,0 +1,41 @@
+const API_KEY = '9bd7ca1a7f5c5827cc2e1e4022bfb8e9';
+
+const fetchData = position => {
+    const {latitude,longitude} = position.coords;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => setWeatherData(data));
+}
+
+const setWeatherData = data => {
+    console.log(data);
+    const weatherData ={
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate(),
+    }
+    Object.keys(weatherData).forEach( key =>{
+        document.getElementById(key).textContent = weatherData[key];
+    });
+
+
+    cleanUp();
+}
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${( '0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
+}
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
+
+const cleanUp = () =>{
+    let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none';
+    container.style.display = 'flex'
+}
